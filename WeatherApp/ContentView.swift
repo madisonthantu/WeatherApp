68b95df17cd786c93f95d1e529b4c1ef33beca86
madisonthantu <madisonthantu@gmail.com>
2022-08-25 13:23:55 -0400

    Created opening page and transition to what will be the main page

--- WeatherApp/ContentView.swift
+++ WeatherApp/ContentView.swift
@@ -8,30 +8,78 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var openingPage = true
+    var body: some View {
+        if openingPage {
+            VStack {
+                ZStack {
+                    Image("sun").resizable()
+                        .frame(width: 50.0, height: 50.0)
+                        .offset(x: 15.0, y: -15.0)
+                    Image("skincare").resizable()
+                        .frame(width: 50.0, height: 50.0)
+                        .offset(x: -15.0, y: 10.0)
+                }
+                Text("Sun & Skin")
+                    .multilineTextAlignment(.center)
+                    .padding()
+                    .font(
+                        Font.custom(
+                            "Pacifico",
+                            fixedSize: 34)
+                        .weight(.heavy)
+                    )
+                    .foregroundColor(.white)
+                    .offset(y: -20)
+                Button("Click to continue") {
+                    withAnimation {
+                        openingPage.toggle()
+                    }
+                }
+                .foregroundColor(Color.white)
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(Color.pink)
+        } else {
+            MainPageView()
+        }
+    }
+}
+
+//struct OpeningPageView: View {
+//    var body: some View {
+//        VStack {
+//            ZStack {
+//                Image("sun").resizable()
+//                    .frame(width: 50.0, height: 50.0)
+//                    .offset(x: 15.0, y: -15.0)
+//                Image("skincare").resizable()
+//                    .frame(width: 50.0, height: 50.0)
+//                    .offset(x: -15.0, y: 10.0)
+//            }
+//            Text("Sun & Skin")
+//                .multilineTextAlignment(.center)
+//                .padding()
+//                .font(
+//                    Font.custom(
+//                        "Pacifico",
+//                        fixedSize: 34)
+//                    .weight(.heavy)
+//                )
+//                .foregroundColor(.white)
+//                .offset(y: -20)
+//        }
+//        .frame(maxWidth: .infinity, maxHeight: .infinity)
+//        .background(Color.pink)
+//    }
+//}
+
+struct MainPageView: View {
     var body: some View {
         VStack {
-            ZStack {
-                Image("sun").resizable()
-                    .frame(width: 50.0, height: 50.0)
-                    .offset(x: 15.0, y: -15.0)
-                Image("skincare").resizable()
-                    .frame(width: 50.0, height: 50.0)
-                    .offset(x: -15.0, y: 10.0)
-            }
-            Text("Sun & Skin")
-                .multilineTextAlignment(.center)
-                .padding()
-                .font(
-                    Font.custom(
-                        "Pacifico",
-                        fixedSize: 34)
-                    .weight(.heavy)
-                )
-                .foregroundColor(.white)
-                .offset(y: -20)
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .background(Color.pink)
+        .background(Color.white)
     }
 }
 
